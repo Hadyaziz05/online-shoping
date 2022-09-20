9901137017ac285b432d32bb573e4cf0bd2b9cce
Hady <hadyabdelaziz05@gmail.com>
2022-09-20 09:04:56 +0200

    Changes

--- routers/brows.js
+++ routers/brows.js
@@ -9,14 +9,29 @@ const { Product } = require('../models/products');
 const router = express.Router();
 
 router.get('/', auth , async(req, res)=> {
+    let arr = []
 const product = await Product.find()
 for (let i = 0; i < product.length; i++){
     if (product[i].quantity === 0) {
         product.splice(i, 1)
-    }
+    } 
+}
+for (let j =0; j < product.length; j++) {
+    let user = await User.findOne({_id: product[j].userId})
+    arr.push({
+        sellerDetails: {
+            name: user.name,
+            email: user.email
+        },
+        productDetails: {
+            _id: product[j]._id,
+            name: product[j].nameOfProduct,
+            quantity: product[j].quantity
+        }
+    })
 }
 
-    res.send(product)
+    res.send(arr)
 })
 
 
