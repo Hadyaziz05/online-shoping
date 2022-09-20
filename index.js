f192205f742f866b79ff7d8d7394c441d027fc68
Hady <hadyabdelaziz05@gmail.com>
2022-09-20 08:28:38 +0200

    Changes

--- index.js
+++ index.js
@@ -9,9 +9,8 @@ const express = require('express');
 const app = express();
 
 
-mongoose.connect(config.get('db'))
+mongoose.connect('mongodb+srv://olxUser:1234@mycluster.nt0jizn.mongodb.net/test')
   .then(() => console.log('Connected to MongoDB...'))
-  .catch(err => console.error('Could not connect to MongoDB...'));
 
 app.use(express.json());
 
