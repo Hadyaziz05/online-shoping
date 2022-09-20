b37586ab4b0c355b298fa1631213d9aaad48e211
Hady <hadyabdelaziz05@gmail.com>
2022-09-20 07:55:50 +0200

    Changes to the app

--- startup/db.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-const config = require('config')
-module.exports = function() {
-    mongoose.connect('mongodb+srv://olxUser:1234@cluster0.63uvqr4.mongodb.net/test')
-  .then(() => console.log('Connected to MongoDB...'))
-  
-}
